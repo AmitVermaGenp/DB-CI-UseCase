c44e6bc3cd1d9892021f5d50082b59aa7f2da167
AmitVermaGenp <amit.verma2e84c5@genpact.com>
2018-05-01 12:55:21 +0530

    FDI-73 Test AM 2

--- 400221366-devops-genpact-06539bab44e3/examples/feed-combiner-java8-webapp/src/main/java/org/intillegen/IntelliGen.java
+++ 400221366-devops-genpact-06539bab44e3/examples/feed-combiner-java8-webapp/src/main/java/org/intillegen/IntelliGen.java
@@ -47,9 +47,9 @@ public class IntelliGen extends HttpServlet {
 			String name= request.getParameter("name");
 			String email= request.getParameter("email");
 			String message = request.getParameter("message");
-			//String phone_number = request.getParameter("phone_number");
-			//String query= "insert into user_info( name , email, message, phone_number) values(  '"+name+"' , '"+email+"' , '"+message+"','"+phone_number+"')";
-			String query= "insert into user_info( name , email, message) values(  '"+name+"' , '"+email+"' , '"+message+"')";
+			String phone_number = request.getParameter("phone_number");
+			String query= "insert into user_info( name , email, message, phone_number) values(  '"+name+"' , '"+email+"' , '"+message+"','"+phone_number+"')";
+			//String query= "insert into user_info( name , email, message) values(  '"+name+"' , '"+email+"' , '"+message+"')";
 			
 			
 			//String query= "ALTER TABLE user_info ADD phone_number varchar(11)";

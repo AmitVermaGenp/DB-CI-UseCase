62d0b9392144da11e914206663cff86ad612f628
AmitVermaGenp <amaresh.mishra@genpact.com>
2019-06-21 19:29:36 +0530

    ip
    ip

--- 400221366-devops-genpact-06539bab44e3/examples/feed-combiner-java8-webapp/src/main/java/org/intillegen/DataServlet.java
+++ 400221366-devops-genpact-06539bab44e3/examples/feed-combiner-java8-webapp/src/main/java/org/intillegen/DataServlet.java
@@ -40,7 +40,7 @@ public class DataServlet extends HttpServlet {
 		try{  
 			
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-			String dbURL = "jdbc:sqlserver://52.66.139.121:1433;databaseName=Intelligen_Dev;";
+			String dbURL = "jdbc:sqlserver://13.234.77.226:1433;databaseName=Intelligen_Dev;";
 			Connection conn =  (Connection) DriverManager.getConnection(dbURL,"intelligenuser", "password" );
 			if (conn != null) {
 			    System.out.println("Connected");
